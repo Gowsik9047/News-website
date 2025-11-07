679085350368667ce8459636eccc7df04c170628
Gowsik9047 <gowsik9047@gmail.com>
2025-11-07 23:05:51 +0530

    header implement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Components/header";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div style={{ padding: "20px" }}>
+        <h1>Welcome to My News Website</h1>
+        <p>Here is some content below the navbar...</p>
+      </div>
     </div>
   );
 }
