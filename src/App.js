53f3bcf7e65cc6d3cdfc8f2f549c5db9eb245d66
Gowsik9047 <gowsik9047@gmail.com>
2025-11-10 19:00:56 +0530

    Main grid and ad implement

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Navbar from "./Components/header";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Home from "./Pages/home";
 
@@ -6,7 +5,6 @@ import Home from "./Pages/home";
 function App() {
   return (
     <div>
-      {/* <Navbar /> */}
       <Home />
     </div>
   );
--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,17 +1,17 @@
 import Navbar from "../Components/Navbar/navbar";
 import LiveTicker from "../Components/LiveTicker/liveticker";
-import NewsGrid from "../Components/NewsGrid/newsgrid";
 import AdBanner from "../Components/AdBanner/adbanner";
 import FeaturedNews from "../Components/FeaturedNews/featurednews";
+import NewsGrid from "../Components/NewsGrid/newsgrid";
 
 function Home() {
   return (
     <>
       <Navbar />
       <LiveTicker />
-      {/* <NewsGrid /> */}
       <FeaturedNews />
       <AdBanner />
+      <NewsGrid />
     </>
   );
 }
